3c2889ba08ccb6419a5bcee619d57ebad763199f
Alex Ferreira <asfuture2019@gmail.com>
2024-08-19 08:27:50 -0300

    Alterando titulo da aba

--- tests/app.component.spec.ts
+++ tests/app.component.spec.ts
@@ -35,7 +35,7 @@ describe('AppComponent', () => {
   it(`should have as title 'clarkeenergia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clarkeenergia');
+    expect(app.title).toEqual('Energia Limpa');
   });
 
   // it('should render title', () => {
